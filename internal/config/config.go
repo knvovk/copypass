fc6e855fe8289ff0e738bdba40cc606ecd586538
knvovk <knvovk@outlook.com>
2022-11-05 22:07:24 +0300

    Изменен конфиг базы данных.

--- internal/config/config.go
+++ internal/config/config.go
@@ -16,11 +16,9 @@ type app struct {
 }
 
 type database struct {
-	Host     string `toml:"host"`
-	Port     int    `toml:"port"`
-	Name     string `toml:"name"`
-	User     string `toml:"user"`
-	Password string `toml:"password"`
+	URL             string `toml:"url"`
+	MaxPoolSize     int32  `toml:"max_pool_size"`
+	MaxConnIdleTime int    `toml:"max_conn_idle_time"`
 }
 
 type logging struct {
