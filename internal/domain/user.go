d2b1f3213c318c580cd88b738b6ead61b845ff8c
knvovk <knvovk@outlook.com>
2022-11-05 23:27:56 +0300

    Добавлена модель пользователя.

---
+++ internal/domain/user.go
@@ -0,0 +1,18 @@
+package domain
+
+type User struct {
+	Id           string
+	Username     string
+	Email        string
+	PasswordHash string
+}
+
+type UserRepository interface {
+	Insert(user User) (*User, error)
+	Find(id string) (*User, error)
+	FindByUsername(username string) (*User, error)
+	FindByEmail(email string) (*User, error)
+	FindAll(limit, offset int) ([]*User, error)
+	Update(user User) error
+	Delete(id string) error
+}
