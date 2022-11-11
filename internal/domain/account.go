74a1c93dd727baad887572ed1716f8d3d94d8076
knvovk <knvovk@outlook.com>
2022-11-11 21:29:08 +0300

    Добавлен репозиторий аккаунта.

--- internal/domain/account.go
+++ internal/domain/account.go
@@ -6,16 +6,16 @@ type Account struct {
 	Name        string
 	Description string
 	Url         string
-	Login       string
+	Username    string
 	Password    string
 }
 
 type AccountRepository interface {
-	Insert(account Account) (Account, error)
+	Insert(a Account) (Account, error)
 	Find(id string) (Account, error)
 	FindByUser(user User) (Account, error)
 	FindByName(name string) (Account, error)
 	FindAll(limit, offset int) ([]Account, error)
-	Update(user Account) (Account, error)
+	Update(a Account) (Account, error)
 	Delete(id string) error
 }
