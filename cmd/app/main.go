449b0e2b2ff873d904a066bd7aec091a766324b0
knvovk <knvovk@outlook.com>
2022-11-20 16:26:29 +0300

    Заменен toml config на os.Getenv.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,54 +2,26 @@ package main
 
 import (
 	"database/sql"
-	"flag"
-	"fmt"
 	"log"
 	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/knvovk/copypass/internal/app"
-	"github.com/knvovk/copypass/internal/config"
-	"github.com/sirupsen/logrus"
 )
 
-var configPath = flag.String("config", "configs/config.toml", "Path to config.toml")
-
 func main() {
-	flag.Parse()
-
-	cfg, err := config.GetConfig(*configPath)
-	if err != nil {
-		fmt.Printf("Can't load config file: %v\n", err)
-		os.Exit(1)
-	}
-
 	flags := os.O_CREATE | os.O_APPEND | os.O_WRONLY
-	file, err := os.OpenFile(cfg.Log.Path, flags, 0666)
+	file, err := os.OpenFile(os.Getenv("LOG_PATH"), flags, 0666)
 	if err != nil {
 		log.Fatalf("Couldn't open the logs file: %v\n", err)
 	}
 	log.SetOutput(file)
 
-	db, err := sql.Open("pgx", cfg.DB.URL)
+	db, err := sql.Open("pgx", os.Getenv("DB_URL"))
 	if err != nil {
-		fmt.Printf("Unable to create connection pool: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("Couldn't connect to database: %v\n", err)
 	}
 	defer db.Close()
 
-	if err = app.Run(cfg, db); err != nil {
-		log.Fatalf("Couldn't run application: %v\n", err)
-	}
-}
-
-func buildLogFormatter(cfg *config.Config) *logrus.JSONFormatter {
-	return &logrus.JSONFormatter{
-		TimestampFormat:   cfg.Log.DateTimeFormat,
-		DisableTimestamp:  false,
-		DisableHTMLEscape: false,
-		FieldMap:          nil,
-		CallerPrettyfier:  nil,
-		PrettyPrint:       false,
-	}
+	app.Run(db)
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -4,39 +4,42 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/knvovk/copypass/internal/transport/rest"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/knvovk/copypass/internal/config"
 	"github.com/knvovk/copypass/internal/service"
 	"github.com/knvovk/copypass/internal/storage"
 )
 
-func Run(cfg *config.Config, db *sql.DB) error {
-	address := fmt.Sprintf("%s:%d", cfg.App.Host, cfg.App.Port)
+func Run(db *sql.DB) {
+	host := os.Getenv("APP_HOST")
+	port := os.Getenv("APP_PORT")
+	address := fmt.Sprintf("%s:%s", host, port)
 	router := mux.NewRouter()
-	{
-		_storage := storage.NewUserStorage(db)
-		_service := service.NewUserService(_storage)
-		_handler := rest.NewUserHandler(_service)
-		_handler.Register(router)
-	}
-	{
-		_storage := storage.NewAccountStorage(db)
-		_service := service.NewAccountService(_storage)
-		_handler := rest.NewAccountHandler(_service)
-		_handler.Register(router)
-	}
 	server := &http.Server{
 		Addr:         address,
 		Handler:      router,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
-	fmt.Printf("Listen on %s\n", address)
-	if err := server.ListenAndServe(); err != nil {
-		return err
+
+	{
+		_storage := storage.NewUserStorage(db)
+		_service := service.NewUserService(_storage)
+		_handler := rest.NewUserHandler(_service)
+		_handler.Register(router)
 	}
-	return nil
+
+	{
+		_storage := storage.NewAccountStorage(db)
+		_service := service.NewAccountService(_storage)
+		_handler := rest.NewAccountHandler(_service)
+		_handler.Register(router)
+	}
+
+	log.Printf("Listen on %s...\n", address)
+	log.Fatal(server.ListenAndServe())
 }
